2f2d4a2d00102701f4e3de198f86ad62047f1b34
oleks <admin@oleks.info>
2011-05-07 00:11:02 +0200

    isolated lexer

--- kiss.py
+++ kiss.py
@@ -2,127 +2,9 @@ import os, re, sys
 
 from collections import deque
 
-rc = re.compile
+import lexer
+from lexer import Lexer
 
-class SyntaxObject(object):
-	def __eq__(self, other):
-		if not other.__class__.__name__ == 'str':
-			return False
-		return self.__class__.__name__ == other
-
-	def __ne__(self, other):
-		return not self.__eq__(other)
-
-class Token(SyntaxObject):
-	def __init__(self, match=None):
-		self.name = self.__class__.__name__
-		if match:
-			self.raw = match.group(0)
-
-	def __str__(self):
-		return self.name
-
-class Newline(Token):
-	pass
-
-class Tab(Token):
-	pass
-
-class Include(Token):
-	def __init__(self, match):
-		super(Include, self).__init__(match)
-		self.libraries = []
-
-	def append(self, element):
-		self.libraries.append(element)
-
-class Class(Token):
-	pass
-
-class LeftBracket(Token):
-	pass
-
-class RightBracket(Token):
-	pass
-
-class LeftBrace(Token):
-	pass
-
-class RightBrace(Token):
-	pass
-
-class Assign(Token):
-	pass
-
-class Dot(Token):
-	pass
-
-class Comma(Token):
-	pass
-
-class MapsTo(Token):
-	def __init__(self, match):
-		super(MapsTo, self).__init__(match)
-		self.parameters = []
-		self.expressions = []
-
-
-class Call(SyntaxObject):
-	def __init__(self, path, args):
-		self.path = path
-		self.args = args
-
-	def __str__(self):
-		return ".".join(self.path)
-
-class Varname(Token):
-	def __init__(self, match):
-		super(Varname, self).__init__(match)
-
-	def __str__(self):
-		return self.name + " : " + self.raw
-
-class Tokens:
-	SPLITTER = rc(r" +")
-
-	ALL = [\
-		(Newline, rc(r"\r?\n")),\
-		(Tab,     rc(r"\t")),\
-		(Include, rc(r"include")),\
-		(Varname, rc(r"[a-zA-Z]+")),\
-		(Class,   rc(r"class")),\
-		(LeftBracket,  rc(r"{")),\
-		(RightBracket, rc(r"}")),\
-		(LeftBrace,    rc(r"\(")),\
-		(RightBrace,   rc(r"\)")),\
-		(Dot,     rc(r"\.")),\
-		(Comma,   rc(r",")),\
-		(MapsTo,  rc("=>")),\
-		(Assign,  rc(r"="))]
-
-class Lexer:
-	def __init__(self, filename):
-		self.tokens = deque()
-		for line in open(filename):
-			self.lex(line)
-
-	def lex(self, line):
-		words = Tokens.SPLITTER.split(line)
-		for word in words:
-			for token in self.tokenize(word):
-				self.tokens.append(token)
-
-	def tokenize(self, word):
-		while len(word) > 0:
-			(token, word) = self.match(word)
-			yield token
-	
-	def match(self, word):
-		for fun, regex in Tokens.ALL:
-			match = regex.match(word)
-			if match:
-				return (fun(match), word[match.end():])
-		raise Exception("Unmatched token %s!" % word)
 
 class Grammar:
 	def __init__(self):
@@ -338,8 +220,8 @@ class Compiler:
 
 def kiss(filename):
 	tokens = Lexer(filename).tokens
-	#for token in tokens:
-	#	print token
+	for token in tokens:
+		print token
 	#syntree = Parser(tokens).syntree
 	
 	#for expression in syntree[1]:
